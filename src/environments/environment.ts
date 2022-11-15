d7595d6bd4e88d9c938023a26d573f742bf3f0ab
rob-digital <robert.roksela@gmail.com>
2022-11-15 17:04:59 +0000

    Final version ready

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // BASE_URL: "http://localhost:6060",
-  BASE_URL: "http://CSEVWEBST25:6060",
+  BASE_URL: "http://localhost:6060",
+  // BASE_URL: "http://CSEVWEBST25:6060",
   // BASE_URL: "http://edinlp0127:6060"
  //  http://csevwebst25:8082/FootballTipping2022
 };
